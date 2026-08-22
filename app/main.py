34ce81aaecb5ee7b2169017a1c937851472747ba
Monish Gandhe <monish@Monishs-MacBook-Pro.local>
2026-08-22 16:44:25 -0700

    Update app for preview environment test

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI(
     title="Cloud Preview Platform",
-    version="1.0.0"
+    version="1.1.0"
 )
 
 @app.get("/")
 def home():
     return {
         "message": "Preview Environment Platform",
-        "version": "1.0.0"
+        "version": "1.1.0"
     }
 
 @app.get("/health")
